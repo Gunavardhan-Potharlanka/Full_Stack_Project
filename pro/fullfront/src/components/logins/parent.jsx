15b9ddeb22a832060caa82e489dfaafda838bdb3
Gunavardhan-Potharlanka <gunavardhanp2004@gmail.com>
2023-10-23 19:15:12 +0530

    Changes made

--- pro/fullfront/src/components/logins/parent.jsx
+++ pro/fullfront/src/components/logins/parent.jsx
@@ -5,7 +5,7 @@ const Parent = () => {
     <div className="card">
         <div className="card-body">
             <h5 className="card-title">Parent</h5>
-            <label htmlFor='usr'>Username</label>
+            <label htmlFor='usr'>Rollno</label>
             <input type='text' name='usr' className='card px-2'/>
             <label htmlFor='pass'>Password</label>
             <input type='text' name='pass' className='card mb-2 px-2'/>
