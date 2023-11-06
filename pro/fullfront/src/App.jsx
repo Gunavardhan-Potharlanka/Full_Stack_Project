4966a31139416a6d4b8390692e9439cbcbd48688
Gunavardhan-Potharlanka <gunavardhanp2004@gmail.com>
2023-11-06 22:15:10 +0530

    11/6/2023 10:14 PM

--- pro/fullfront/src/App.jsx
+++ pro/fullfront/src/App.jsx
@@ -1,6 +1,4 @@
 import React from "react"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import { BrowserRouter, Navigate, Route, Routes} from 'react-router-dom'
 import Login from './components/login'
 import HOD from './components/pages/hodpage'
--- pro/fullfront/src/main.jsx
+++ pro/fullfront/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import '../src/assets/scss/main.scss'
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <App />
